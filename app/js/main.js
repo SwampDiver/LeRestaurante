cf83014d0a8875b3f6915a9b1c1d49d1f93c899b
SwampDiver <toashes88@gmail.com>
2021-04-10 19:50:50 +0700

    slider jquery

--- app/js/main.js
+++ app/js/main.js
@@ -1,46 +1,46 @@
-$(function() {
-	$('.gallery__wrapper').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		}
-	});
+// --- GALLERY ---
+
+$(function () {
+    $('.gallery__wrapper').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1]
+        }
+    });
 });
 
-$(function() {
-    $('.main-nav__burger').click(function(event) {
+// --- MENU ---
+
+$(function () {
+    $('.main-nav__burger').click(function (event) {
         $('.main-nav__burger,.main-nav__list').toggleClass('active');
     });
 });
 
+// --- SLIDER ---
 
+$(function () {
+    let index = 0;
 
-const next = document.querySelector('.online-menu__next-btn');
-const slides = document.querySelectorAll('.online-menu__offer');
+    const activeSlide = n => {
+        for (let slide of $('.online-menu__offer')) {
+            slide.classList.remove('online-menu__offer--active');
+        }
+        $('.online-menu__offer')[n].classList.add('online-menu__offer--active');
+    };
 
-let index = 0;
-
-const activeSlide = n => {
-  for (let slide of slides) {
-    slide.classList.remove('online-menu__offer--active');
-  }
-  slides[n].classList.add('online-menu__offer--active');
-};
-
-const nextSlide = () => {
-  if (index == slides.length - 1) {
-    index = 0;
-    activeSlide(index);
-  } else {
-    index++;
-    activeSlide(index);
-  }
-};
-
-
-next.addEventListener('click', nextSlide);
+    $('.online-menu__next-btn').click(function () {
+        if (index == $('.online-menu__offer').length - 1) {
+            index = 0;
+            activeSlide(index);
+        } else {
+            index++;
+            activeSlide(index);
+        }
+    });
+});
